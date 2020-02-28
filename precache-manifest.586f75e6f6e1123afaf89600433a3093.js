7fcbb0afbdc790d48ec22248976619431f87b308
Dave Hart <davey3000plus@googlemail.com>
2020-02-28 02:59:28 +0000

    - Re-published to GitHub Pages to fix #14 (Fixed paths to resources when building for GitHub Pages with "yarn build").

---
+++ precache-manifest.586f75e6f6e1123afaf89600433a3093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cc5dc2ca290b228a638ab1d7a3119f",
+    "url": "/whats-the-weather/index.html"
+  },
+  {
+    "revision": "df5caddaa9281bbf124a",
+    "url": "/whats-the-weather/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "bf5bb2b4f6b397a4c4eb",
+    "url": "/whats-the-weather/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df5caddaa9281bbf124a",
+    "url": "/whats-the-weather/static/js/2.cdcf3282.chunk.js"
+  },
+  {
+    "revision": "a108bd14c69faf1507e1074a6c8a5de4",
+    "url": "/whats-the-weather/static/js/2.cdcf3282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5bb2b4f6b397a4c4eb",
+    "url": "/whats-the-weather/static/js/main.ab920f2b.chunk.js"
+  },
+  {
+    "revision": "ae9b992b39ae3831a8a6",
+    "url": "/whats-the-weather/static/js/runtime-main.2c74523e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20db1c813445d95b2d4d5333b637ed25.js"
+  "/whats-the-weather/precache-manifest.586f75e6f6e1123afaf89600433a3093.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whats-the-weather/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
