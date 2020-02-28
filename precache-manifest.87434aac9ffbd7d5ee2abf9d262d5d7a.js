e78fca6b108484ad83eafed3d5a650d479188915
Dave Hart <davey3000plus@googlemail.com>
2020-02-28 01:29:28 +0000

    - Resolves #6: Public release v1 build created for GitHub pages.

---
+++ precache-manifest.87434aac9ffbd7d5ee2abf9d262d5d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6cf225333465342ce1e67920eeed17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80935ced67465078e55d",
+    "url": "/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "bb0327175928e0001292",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "80935ced67465078e55d",
+    "url": "/static/js/2.bc06985d.chunk.js"
+  },
+  {
+    "revision": "a108bd14c69faf1507e1074a6c8a5de4",
+    "url": "/static/js/2.bc06985d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0327175928e0001292",
+    "url": "/static/js/main.8e631fcd.chunk.js"
+  },
+  {
+    "revision": "079e8323217e8c253e1a",
+    "url": "/static/js/runtime-main.87f0263c.js"
+  }
+]);
